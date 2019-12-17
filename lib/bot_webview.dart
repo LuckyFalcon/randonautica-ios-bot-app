37a76ebadfe212a49a854f2b9c5b88e9553bb0b8
Simon McCorkindale <simon@funx.jp>
2019-12-17 14:21:49 +0900

    Implement basic webview for bot

---
+++ lib/bot_webview.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'package:webview_flutter/webview_flutter.dart';
+
+class BotWebView extends StatelessWidget {
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+
+  BotWebView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Fatumbot"),
+        ),
+        body: WebView(
+          initialUrl: "https://devbot.randonauts.com",
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+        ));
+  }
+}
