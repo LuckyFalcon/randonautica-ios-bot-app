def675ba14f40056790b181d120e78c16c3eb8f1
Simon McCorkindale <simon@funx.jp>
2019-12-23 22:49:21 +0900

    Basic callback function to receive from bot the number of entropy bytes camrng needs to generate

--- lib/bot_webview.dart
+++ lib/bot_webview.dart
@@ -18,6 +18,14 @@ class BotWebView extends StatelessWidget {
         body: WebView(
           initialUrl: "https://devbot.randonauts.com/devbot.html?src=ios",
           javascriptMode: JavascriptMode.unrestricted,
+          javascriptChannels: Set.from([
+            JavascriptChannel(
+                name: 'flutterChannel_loadCamRNGWithBytesNeeded',
+                onMessageReceived: (JavascriptMessage message) {
+                  print(message.message);
+                }),
+            // we can have more than one channels
+          ]),
           onWebViewCreated: (WebViewController webViewController) {
             _controller.complete(webViewController);
           },
