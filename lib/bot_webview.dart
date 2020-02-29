11df376f33fbf2d76904dfe079b896da57203997
Simon McCorkindale <simon@funx.jp>
2020-02-29 20:52:54 +0900

    Fixes to load maps/routes in Google/Apple Maps, and Spotify and Twitter links in their native apps too

--- lib/bot_webview.dart
+++ lib/bot_webview.dart
@@ -38,7 +38,68 @@ class BotWebView extends StatelessWidget {
   }
 
   _launchURL(String url) async {
+    await launch(url);
+  }
+
+  _openMap(String url) async {
+    var isStreetView = false;
+    var isChain = false;
+    var coords = url;
+    if (url.contains("map_action=pano")) { // Street View URL
+      isStreetView = true;
+      coords = coords.replaceAll(
+          "https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=", "")
+          .replaceAll("&fov=90&heading=235&pitch=10", "");
+    } else if (url.contains("maps/dir")) { // Multiple points route
+      // Get all points' coordinates from the URL
+      isChain = true;
+      coords = url.replaceAll("https://www.google.com/maps/dir/", "").replaceAll("+", ",").replaceAll("/", "+to:");
+    } else { // Normal Maps URL
+      coords = coords.replaceAll("https://www.google.com/maps/place/", "").replaceAll("+", ",");
+      coords = coords.substring(0, coords.indexOf("@")-1);
+    }
+
+    var googleMapsUrl = url.replaceAll("https://", "comgooglemaps://");
+    if (await canLaunch(googleMapsUrl)) {
+      // Open in Google Maps
+      if (isStreetView) {
+        await launch("comgooglemaps://?center=" + coords + "&mapmode=streetview");
+      } else if (isChain) { // Multiple points (using the Chains feature)
+        var route = "comgooglemaps://?daddr=${coords}&directionsmode=driving".replaceAll("+to:&", "&");
+        await launch(route);
+      } else { // Normal map view for a point
+        await launch("comgooglemaps://?q=${coords}&center=${coords}&zoom=14&mapmode=standard");
+      }
+    } else {
+      if (isStreetView) {
+        await launch(url); // Street View in Webview
+      } else {
+        // Fall back to Apple Maps by extracting our lat/long from the URL
+        await launch("http://maps.apple.com/?daddr=" + coords);
+      }
+    }
+  }
+
+  _openSpotify(String url) async {
+    var spotifyAppUrl = url.replaceAll("https://open.spotify.com/", "spotify://");
+    if (await canLaunch(spotifyAppUrl)) {
+      // Open in Spotify app
+      await launch(spotifyAppUrl);
+    } else {
+      // Spotify online
       await launch(url);
+    }
+  }
+
+  _openTwitter(String url) async {
+    var twitterAppUrl = url.replaceAll("https://twitter.com/intent/tweet?text=", "twitter://post?message=");
+    if (await canLaunch(twitterAppUrl)) {
+      // Open in Twitter app
+      await launch(twitterAppUrl);
+    } else {
+      // Twitter online
+      await launch(url);
+    }
   }
 
   _initOneSignal() async {
@@ -78,32 +139,55 @@ class BotWebView extends StatelessWidget {
 //          title: Text("Randonautica"),
 //        ),
         body: WebView(
-          initialUrl: botUrl,
-          javascriptMode: JavascriptMode.unrestricted,
-          javascriptChannels: Set.from([
-            JavascriptChannel(
-                name: 'flutterChannel_loadCamRNGWithBytesNeeded',
-                onMessageReceived: (JavascriptMessage message) {
-                  _navToCamRNG(int.parse(message.message)); // open swift TrueEntropy Camera RNG view
-                }),
-            // we can have more than one channels
-          ]),
-          onWebViewCreated: (WebViewController webViewController) {
-            webView = webViewController;
-            _controller.complete(webViewController);
-          },
-          onPageFinished: (String page) {
-            if (page.contains("index.html")){
-              _initOneSignal();
+            initialUrl: botUrl,
+            javascriptMode: JavascriptMode.unrestricted,
+            javascriptChannels: Set.from([
+              JavascriptChannel(
+                  name: 'flutterChannel_loadCamRNGWithBytesNeeded',
+                  onMessageReceived: (JavascriptMessage message) {
+                    _navToCamRNG(int.parse(message.message)); // open swift TrueEntropy Camera RNG view
+                  }),
+              // we can have more than one channels
+            ]),
+            onWebViewCreated: (WebViewController webViewController) {
+              webView = webViewController;
+              _controller.complete(webViewController);
+            },
+            onPageFinished: (String page) {
+              if (page.contains("index.html")){
+                _initOneSignal();
+              }
+            },
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url.startsWith("https://www.google.com/maps/place/") ||
+                  request.url.startsWith("https://www.google.com/maps/dir/") ||
+                  request.url.startsWith("https://www.google.com/maps/@?api=1") ||
+                  request.url.startsWith("https://open.spotify.com/") ||
+                  request.url.startsWith("https://twitter.com")
+              ) {
+                if (request.url.startsWith("https://www.google.com/maps")) {
+                  _openMap(request.url);
+                  return NavigationDecision.prevent;
+                }
+
+                if (request.url.startsWith("https://open.spotify.com/")) {
+                  _openSpotify(request.url);
+                  return NavigationDecision.prevent;
+                }
+
+                if (request.url.startsWith("https://twitter.com")) {
+                  _openTwitter(request.url);
+                  return NavigationDecision.prevent;
+                }
+
+                _launchURL(request.url);
+                return NavigationDecision.prevent;
+              } else if (!request.url.startsWith(botUrl)) {
+                _launchURL(request.url);
+                return NavigationDecision.prevent;
+              }
+              return NavigationDecision.navigate;
             }
-          },
-          navigationDelegate: (NavigationRequest request) {
-            if (!request.url.startsWith(botUrl)) {
-              _launchURL(request.url);
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          }
         ));
   }
 }
